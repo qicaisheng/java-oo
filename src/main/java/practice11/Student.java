402a677b1ea65411b406c6e6b98739c1553e421f
qicaisheng <qicaisheng@qq.com>
2020-04-05 18:12:34 +0800

    impl practice11, ignore notified

--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,36 @@
 package practice11;
 
-public class Student {
+public class Student extends Person {
+    private final Klass klass;
+    
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String isLeaderIntroduce = isLeaderOf(klass) ? "Leader of " : "at ";
+        return super.introduce() + " I am a Student. I am " + isLeaderIntroduce + "Class " + getKlass().getNumber() + ".";
+    }
+
+    public boolean isLeaderOf(Klass klass) {
+        if (klass.getLeader() == null) {
+            return false;
+        }
+        return this.equals(klass.getLeader());
+    }
+
+    public boolean isIn(Klass klass) {
+        return getKlass().equals(klass);
+    }
 }
