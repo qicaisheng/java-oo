c0af1ce9e2ad4310024deba849357bba2d114750
qicaisheng <qicaisheng@qq.com>
2020-04-05 16:21:55 +0800

    impl practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int classNumber;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int classNumber) {
+        super(name, age);
+
+        this.classNumber = classNumber;
+    }
+
+    public int getKlass() {
+        return classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        String teachClassString = getKlass() == 0 ? "No Class." : "Class " + getKlass() + ".";
+        return super.introduce() + " I am a Teacher. I teach " + teachClassString;
+    }
 }
