c475fa4d68b3ac612e8bb45c726698724c07a2ef
qicaisheng <qicaisheng@qq.com>
2020-04-05 16:09:16 +0800

    impl practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,24 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int classNumber;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+
+        this.classNumber = classNumber;
+    }
+
+    public int getKlass() {
+        return classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
