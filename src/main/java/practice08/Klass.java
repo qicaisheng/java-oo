4aa66794e0ab0b28e3e435da7708ebbaba50d10a
qicaisheng <qicaisheng@qq.com>
2020-04-05 17:21:04 +0800

    impl practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,43 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number &&
+                Objects.equals(leader, klass.leader);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, leader);
+    }
+
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,46 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String teachClassString = getKlass() != null ? "Class " + getKlass().getNumber() + "." : "No Class.";
+        return super.introduce() + " I am a Teacher. I teach " + teachClassString;
+    }
+
+    public String introduceWith(Student student) {
+        String introduceWithStudent = isTeach(student) ? "I teach " + student.getName() : "I don't teach " + student.getName();
+        return super.introduce() + " I am a Teacher. " + introduceWithStudent + ".";
+    }
+
+    private boolean isTeach(Student jerry) {
+        if (getKlass() == null || jerry.getKlass() == null) {
+            return false;
+        }
+        return jerry.getKlass().getNumber() == this.getKlass().getNumber();
+    }
 }
