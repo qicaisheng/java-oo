bf2d598262e250dd4916ce9db9abd3ff07e9df9b
qicaisheng <qicaisheng@qq.com>
2020-04-05 16:38:39 +0800

    impl practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,23 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + getKlass().getNumber() + ".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,31 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String teachClassString = getKlass() == null ? "No Class." : "Class " + getKlass().getNumber() + ".";
+        return super.introduce() + " I am a Teacher. I teach " + teachClassString;
+    }
+
+    public String introduceWith(Student jerry) {
+        boolean teachJerry = getKlass() != null && (jerry.getKlass().getNumber() == this.getKlass().getNumber());
+        String introduceWithJerry = teachJerry ? "I teach " + jerry.getName() : "I don't teach " + jerry.getName();
+        return super.introduce() + " I am a Teacher. " + introduceWithJerry + ".";
+    }
 }
